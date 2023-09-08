0d638beb48ed2f4f1810432c21b896d9030947cf
a.kalyta <a.kalyta@program-ace.com>
2023-09-08 16:24:26 +0300

    GameScene

---
+++ Assets/Scripts/ActionHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Elympics;
+using UnityEngine;
+
+public class ActionHandler : MonoBehaviour, IObservable
+{
+    [SerializeField] private AttackHandler attackHandler;
+    [SerializeField] private ElympicsInt currentAction = new ElympicsInt();//0 - idle, 1 - attack
+
+    [SerializeField] private int attackCooldown;
+
+    private long attackAvailableTick = 0;
+
+    public int GetCurrentAction()
+    {
+        return currentAction.Value;
+    }
+
+    public void HandleActions(bool attack, long tick)
+    {
+        if (attack && tick > attackAvailableTick)
+        {
+            attackHandler.Attack(tick);
+            attackAvailableTick = tick + attackCooldown;
+        }
+        
+    }
+}
